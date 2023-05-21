1ed8a0582fbcca769c6e1afdd1045e82d8309129
kevin-Illu <78170908+Kevin-Illu@users.noreply.github.com>
2023-05-21 11:25:16 -0600

    Initial commit

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,62 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+const withContainer = (WrappedComponent) => {
+  return (props) => {
+    return (
+      <div className="container">
+        <WrappedComponent {...props} />
+      </div>
+    );
+  };
+};
+const HomePageContent = () => <h1>Home</h1>;
+const UsersPageContent = () => <h1>Users</h1>;
+const AboutPageContent = () => <h1>About</h1>;
+
+const HomePage = withContainer(HomePageContent);
+const UsersPage = withContainer(UsersPageContent);
+const AboutPage = withContainer(AboutPageContent);
+
+export const Navigation = () => {
+  const navLinkIsActive = ({ isActive }: { isActive: boolean }) => {
+    return isActive ? "link-active" : "";
+  };
+
+  return (
+    <BrowserRouter>
+      <div>
+        <nav className="nav">
+          <div className="nav-root container">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+            <ul>
+              <li>
+                <NavLink to="/home" className={navLinkIsActive}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" className={navLinkIsActive}>
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/users" className={navLinkIsActive}>
+                  Users
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="about" element={<AboutPage />} />
+          <Route path="users" element={<UsersPage />} />
+          <Route path="home" element={<HomePage />} />
+
+          <Route path="/*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
